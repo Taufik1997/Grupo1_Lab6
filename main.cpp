c23ba3af62825e6833b0533df3288516bcb3ddda
Jorge Morazan <jorgemorazan@unitec.edu>
2016-11-25 14:21:02 -0600

    main empezar

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(){
 		cout<<"     Football\n1)Agregar un nuevo Equipo\n2)Crear nuevos Jugadores o Entrenadores\n3)Formar Equipo\n4)Simulador\n5)¿Cómo se juega?\n6)Alistar los Equipos\n 7)Salir";
 		cin>>opc;
 		if(opc==1){
-			string nombreEquipo, nombreEntrenador, apellidoEntrenador, nombreJugador;
+			string nombreEquipo, nombreEntrenador, apellidoEntrenador, nombreJugador,sobrenombre;
 			int nivel;
 			cout<<""<<endl;//Explicación aquí
 			cout<<"¡Ingrese el nombre de su gran equipo!: ";
@@ -18,7 +18,16 @@ int main(){
 			cin>>nombreEntrenador;
 			cout<<"Ingrese el apellido del entrenador: ";
 			cin>>apellidoEntrenador;
-			
+			cout<<"Ingrese el sobrenombre del entrenador: ";
+			cin>>sobrenombre;
+			do{
+				cout<<"Ingrese el nivel de este entrenador (1-10): ";
+				cin>>nivel;
+				if(nivel<0 && nivel>10){
+					cout<<"El nivel tiene que estar en el rango 1-10"<<endl;
+				}
+			}while(nivel<0 && nivel>10);
+			cout<<"";
 		}//crea nuevo equipo
 		if(opc==2){
 
